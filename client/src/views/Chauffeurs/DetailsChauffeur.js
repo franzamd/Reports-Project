60f86b3f9625506ee5df796c4d7e6d98b016c860
franz medrano <franz.medrano@icloud.com>
2020-02-28 00:48:20 -0400

    Updated DetailsChauffeur & DetailsVehicle

--- client/src/views/Chauffeurs/DetailsChauffeur.js
+++ client/src/views/Chauffeurs/DetailsChauffeur.js
@@ -35,66 +35,57 @@ const DetailsChauffeur = props => {
     <Container>
       <Card>
         <Row>
-          <Col lg={4}>
+          <Col lg={4} className="m-2">
             <CardImg
               src={img1}
-              className="rounded mx-auto d-block m-2"
+              className="rounded mx-auto d-block"
               style={{
                 maxWidth: "300px",
                 maxHeight: "400px"
               }}
             />
           </Col>
-          <Col lg={6}>
+          <Col lg={8}>
             <CardBody>
               <CardTitle className="text-center">
                 Información del Chofer
               </CardTitle>
-              <div className="d-flex">
-                <div className="col text-center">
-                  <label className="mr-3">Nombres:</label>
-                  <p className="d-inline">{name}</p>
-                </div>
-                <div className="col text-center">
-                  <label className="mr-3">Apellidos:</label>
-                  <p className="d-inline">{lastname}</p>
-                </div>
+              <div className="d-flex col">
+                <label className="mr-3">Nombres:</label>
+                <p className="d-inline">{name}</p>
               </div>
-              <div className="d-flex">
-                <div className="col text-center">
-                  <label className="mr-3">C.I.:</label>
-                  <p className="d-inline">{ci}</p>
-                </div>
-                <div className="col text-center">
-                  <label className="mr-3">Expedido:</label>
-                  <p className="d-inline">{issued}</p>
-                </div>
+              <div className="d-flex col">
+                <label className="mr-3">Apellidos:</label>
+                <p className="d-inline">{lastname}</p>
               </div>
-              <div className="d-flex">
-                <div className="col text-center">
-                  <label className="mr-3">Licencia/Brevet:</label>
-                  <p className="d-inline">{license}</p>
-                </div>
-                <div className="col text-center">
-                  <label className="mr-3">Estado:</label>
-                  <p
-                    className={classnames("d-inline", {
-                      "text-success": state,
-                      "text-danger": !state
-                    })}
-                  >
-                    {state ? "Activo" : "Inactivo"}
-                  </p>
-                </div>
+              <div className="d-flex col">
+                <label className="mr-3">C.I.:</label>
+                <p className="d-inline">{ci}</p>
               </div>
-              <div className="d-flex">
-              <div className="col text-center">
-                  <label className="mr-3">Dirección:</label>
-                  <p className="d-inline">{address}</p>
-                </div>
+              <div className="d-flex col">
+                <label className="mr-3">Expedido:</label>
+                <p className="d-inline">{issued}</p>
               </div>
-              <div className="d-flex"></div>
-              <div className="col text-center font-weight-bold">
+              <div className="d-flex col">
+                <label className="mr-3">Licencia/Brevet:</label>
+                <p className="d-inline">{license}</p>
+              </div>
+              <div className="d-flex col">
+                <label className="mr-3">Estado:</label>
+                <p
+                  className={classnames("d-inline", {
+                    "text-success": state,
+                    "text-danger": !state
+                  })}
+                >
+                  {state ? "Activo" : "Inactivo"}
+                </p>
+              </div>
+              <div className="d-flex col">
+                <label className="mr-3">Dirección:</label>
+                <p className="d-inline">{address}</p>
+              </div>
+              <div className="d-flex col font-weight-bold">
                 <label className="mr-3">Ultimo Registro:</label>
                 <p className="d-inline">
                   {moment(createdAt).format("DD MMMM YYYY, h:mm:ss a")}
--- client/src/views/Vehicles/DetailsVehicle.js
+++ client/src/views/Vehicles/DetailsVehicle.js
@@ -34,10 +34,10 @@ const DetailsVehicle = props => {
     <Container>
       <Card>
         <Row>
-          <Col lg={4}>
+          <Col lg={4} xs className="m-2">
             <CardImg
               src={img1}
-              className="rounded mx-auto d-block m-2"
+              className="rounded mx-auto d-block"
               style={{
                 maxWidth: "300px",
                 maxHeight: "400px"
@@ -49,45 +49,38 @@ const DetailsVehicle = props => {
               <CardTitle className="text-center">
                 Información del Vehiculo
               </CardTitle>
-              <div className="d-flex">
-                <div className="col text-center">
-                  <label className="mr-3">Medio de transporte:</label>
-                  <p className="d-inline">{transport}</p>
-                </div>
-                <div className="col text-center">
-                  <label className="mr-3">Color:</label>
-                  <p className="d-inline">{color}</p>
-                </div>
+              <div className="d-flex col">
+                <label className="mr-3">Medio de transporte:</label>
+                <p className="d-inline">{transport}</p>
               </div>
-              <div className="d-flex">
-                <div className="col text-center">
-                  <label className="mr-3">Número de placa:</label>
-                  <p className="d-inline">{number}</p>
-                </div>
-                <div className="col text-center">
-                  <label className="mr-3">Marca:</label>
-                  <p className="d-inline">{brand}</p>
-                </div>
+              <div className="d-flex col">
+                <label className="mr-3">Color:</label>
+                <p className="d-inline">{color}</p>
               </div>
-              <div className="d-flex">
-                <div className="col text-center">
-                  <label className="mr-3">Volumen:</label>
-                  <p className="d-inline">{volume}</p>
-                </div>
-                <div className="col text-center">
-                  <label className="mr-3">Estado:</label>
-                  <p
-                    className={classnames("d-inline", {
-                      "text-success": state,
-                      "text-danger": !state
-                    })}
-                  >
-                    {state ? "Activo" : "Inactivo"}
-                  </p>
-                </div>
+              <div className="d-flex col">
+                <label className="mr-3">Número de placa:</label>
+                <p className="d-inline">{number}</p>
               </div>
-              <div className="d-flex"></div>
-              <div className="col text-center font-weight-bold">
+              <div className="d-flex col">
+                <label className="mr-3">Marca:</label>
+                <p className="d-inline">{brand}</p>
+              </div>
+              <div className="d-flex col">
+                <label className="mr-3">Volumen:</label>
+                <p className="d-inline">{volume}</p>
+              </div>
+              <div className="d-flex col">
+                <label className="mr-3">Estado:</label>
+                <p
+                  className={classnames("d-inline", {
+                    "text-success": state,
+                    "text-danger": !state
+                  })}
+                >
+                  {state ? "Activo" : "Inactivo"}
+                </p>
+              </div>
+              <div className="d-flex col font-weight-bold">
                 <label className="mr-3">Ultimo Registro:</label>
                 <p className="d-inline">
                   {moment(createdAt).format("DD MMMM YYYY, h:mm:ss a")}
